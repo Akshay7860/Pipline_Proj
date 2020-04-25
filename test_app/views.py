d57439c16f44fd0a33a3b13dcd2b4b6ffec7a433
Akshay Mourya <akshay.mourya@audetemi.com>
2020-04-25 10:32:23 +0530

    Added code into master

---
+++ test_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Create your views here.
+
+class TestView(APIView):
+
+    def post(self, request, format=None):
+        try:
+            data = request.data
+            print (data)
+            a = data.get("a")
+            b = data.get("b")
+            return Response({"Total":int(int(a)+int(b))},status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"msg":str(e)})
+
+
+
+
+
+
